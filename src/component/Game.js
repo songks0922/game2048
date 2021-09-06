2117763ed1535a130197f5734bc74910ddf894fe
songks0922 <skj0922@kookmin.ac.kr>
2021-09-06 10:42:58 +0900

    feat: 키보드를 누름에 따라 타일 추가 및 merge

--- src/component/Game.js
+++ src/component/Game.js
@@ -2,28 +2,27 @@ import React, { useState } from 'react';
 import times from 'lodash/times';
 import { MAX_POS } from '../constant';
 import { getInitialTileList } from '../utill/tile';
+import useMoveTile from '../hook/useMoveTile';
+import Tile from './Tile';
 
 export default function Game() {
-  const [titleList, setTitleList] = useState(getInitialTileList);
+  const [tileList, setTileList] = useState(getInitialTileList);
+  useMoveTile({ tileList, setTileList });
   return (
     <div className="game-container">
       <div className="grid-container">
-        {times(MAX_POS, () => (
-          <div className="grid-row">
-            {times(MAX_POS, () => (
-              <div className="grid-cell"></div>
+        {times(MAX_POS, (index) => (
+          <div key={index} className="grid-row">
+            {times(MAX_POS, (index2) => (
+              <div key={index2} className="grid-cell"></div>
             ))}
           </div>
         ))}
       </div>
 
       <div className="tile-container">
-        {titleList.map((item) => (
-          <div
-            className={`tile tile-${item.value} tile-position-${item.x}-${item.x}`}
-          >
-            <div className="tile-inner">{item.value}</div>
-          </div>
+        {tileList.map((item) => (
+          <Tile key={item.id} {...item} />
         ))}
       </div>
     </div>
