5bd26b22a2121c19bf17a1f178a5bf489df38b4d
songks0922 <skj0922@kookmin.ac.kr>
2021-09-06 10:42:08 +0900

    feat: 키보드 입력 구현

--- src/utill/keyboard.js
+++ src/utill/keyboard.js
@@ -4,11 +4,17 @@ const observerMap = {};
 export function addKeyObserver(key, callback) {
   if (!observerMap[key]) {
     observerMap[key] = [];
-    hotkeys(key, () => {});
+    hotkeys(key, () => excuteCallbacks(key));
   }
   observerMap[key].push(callback);
 }
 
 export function removeKeyObserver(key, callback) {
   observerMap[key] = observerMap[key].filter((item) => item !== callback);
+}
+
+function excuteCallbacks(key) {
+  for (const ob of observerMap[key]) {
+    ob();
+  }
 }
